a54351c7a174f42e1242a004b701b4f37ef1263c
tylergeorges <tmg320v@gmail.com>
2023-04-14 00:56:38 -0500

    Fixed TaskBar Error

--- src/components/TaskBar.tsx
+++ src/components/TaskBar.tsx
@@ -1,16 +1,15 @@
 import { ProfileSvg } from "../svg/ProfileSvg";
-import { ProfileIcon } from "./DesktopIcon/ProfileIcon";
 import { Editor } from "./PixelEditor/Editor";
 
 export const TaskBar = () => {
   return (
     <div
       id="taskbar"
-      className="w-full bg-pink h-20 flex flex-row items-center justify-start px-6"
+      className="flex h-20 w-full flex-row items-center justify-start bg-pink px-6"
     >
       {/* <Editor cell_size={10} element_id="taskbar" /> */}
 
-        <ProfileSvg size={6}/>
+      <ProfileSvg size={6} />
     </div>
   );
 };
