d9137a140a88d84427b8cdbc6b2158f7b67707e5
tylergeorges <tmg320v@gmail.com>
2023-04-18 14:36:45 -0500

    Updated TaskBar

--- src/components/ui/TaskBar/TaskBar.tsx
+++ src/components/ui/TaskBar/TaskBar.tsx
@@ -4,7 +4,6 @@ import { useAppDispatch, useAppSelector } from "@hooks";
 import { selectUser, setCurrentProfile } from "@mainslice";
 import { useCallback } from "react";
 import { glixelApi } from "@util";
-//! old height  h-20
 export function TaskBar() {
   const router = useRouter();
   function handleSignOut() {
@@ -18,11 +17,14 @@ export function TaskBar() {
   const handleProfileIconClick = useCallback(
     async function (e: React.SyntheticEvent) {
       e.preventDefault();
-      const get_users_posts = (await glixelApi("/users/:user_id/posts").get({
-        user_id: user.id,
-      })) as Glixel.Post[];
 
-      dispatch(setCurrentProfile({ posts: get_users_posts, user: user }));
+      if (user) {
+        const get_users_posts = (await glixelApi("/users/:user_id/posts").get({
+          user_id: user.id,
+        })) as Glixel.Post[];
+
+        dispatch(setCurrentProfile({ posts: get_users_posts, user: user }));
+      }
     },
     [user, dispatch]
   );
@@ -31,17 +33,12 @@ export function TaskBar() {
       id="taskbar"
       className="flex h-20 w-full flex-row items-center justify-start border-b-4 border-dark bg-pink px-6"
     >
-      {/* <Editor cell_size={10} element_id="taskbar" /> */}
-
       <button
         onClick={handleProfileIconClick}
         className="clickable   mr-6 text-2xl text-dark"
       >
         Profile
-        {/* <ProfileIcon className={"w-10 clickable"} /> */}
       </button>
-
-      {/* <button className="clickable w-20">Profile</button> */}
 
       <button
         className="clickable text-2xl   text-dark"
